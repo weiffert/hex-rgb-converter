71446c6dc4ab847d66c530b777b7eb88fc0a0046
weiffert <weiffert@gmail.com>
2018-05-18 21:22:28 -0400

    Basic failed number conversion

--- script.js
+++ script.js
@@ -23,11 +23,11 @@ const app = {
     handleHexInput(event) {
         const hex = this.hex.value;
         if(hex.length > 6) {
-            const colors = [hex.substr(1, 2), hex.substr(3, 2), hex.substr(5, 2)];
-            colors = colors.map(color => convertToBaseTen(parseInt(color)));
+            let colors = [hex.substr(1, 2), hex.substr(3, 2), hex.substr(5, 2)];
+            colors = colors.map(color => this.convertToBaseTen(color));
 
             this.rgb.value = `rgb(${colors[0]}, ${colors[1]}, ${colors[2]})`;
-            updateCSS();
+            this.updateCSS();
         }
         if(hex.length === 4) {
 
@@ -35,14 +35,15 @@ const app = {
     },
 
     handleRGBInput(event) {
-        const rgb = this.rgb.value;
-        rgb = rgb.substring(4, rgb.length - 1);
+        const rgb = this.rgb.value
+            .substring(4, rgb.length - 1);
 
         const colors = rgb.split(', ');
-        colors = colors.map(color => convertToHex(parseInt(color)));
+            debugger;
+        colors = colors.map(color => this.convertToHex(parseInt(color)));
 
         this.hex.value = `#${colors[0]}${colors[1]}${colors[2]}`;
-        updateCSS();
+        this.updateCSS();
     },
 
     handleColorInput(event) {
@@ -54,20 +55,38 @@ const app = {
     },
 
     convertToBaseTen(value) {
-        if(value < 0) 
-            return 0;
-        else {
-            return value / 10 + 10 * this.convertToBaseTen(value / 10);
+        let ten = 0;
+        value = value.toUpperCase();
+        for(let i = 0; i < value.length; i++) {
+            console.log("ten: " + ten);
+            ten *= 16;
+            if(value.charAt(i) >= '0' && value.charAt(i) <= '9') {
+                ten += (value.charAt(i) - '0') * 16;
+            }
+            else if(value.charAt(i) >= 'A' && value.charAt(i) <= 'F') {
+                ten += (value.charAt(i) - 'A' + 10) * 16;
+            } else {
+                console.error("Invalid character");
+            }
         }
     },
 
     convertToHex(value) {
         let hex = "";
         while(value > 0) {
-            hex = value % 16 + hex;
+            const digit = value % 16 + hex;
+            if(digit >= 10) {
+                hex += ('A' + digit - 10);
+            } else {
+                hex += digit.toString();
+            }
             value /= 16;
         }
         return hex;
+    },
+
+    updateCSS() {
+
     },
 }
 
